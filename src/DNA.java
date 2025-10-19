bb3409c7d42ef028c0c2b68b3dfa2a309f825c96
L-Tran <logan.tran@menloschool.org>
2025-10-19 16:33:19 -0700

    Started on final + Worklog 3

--- src/DNA.java
+++ src/DNA.java
@@ -6,40 +6,48 @@
  * at Menlo School in Atherton, CA
  *</p>
  * <p>
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Logan Tran
  *</p>
  */
 
 public class DNA {
 
-    /**
-     * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
-     */
+    public static final int EXTENDED_ASCII = 256;
+    public static final int P = 2147483647;
+
+
     public static int STRCount(String sequence, String STR) {
-        int i = 0;
+        int m = STR.length();
+        int n = sequence.length();
+
+        int strHash = hash(STR);
+        int seqHash = hash(sequence.substring(0, m - 1));
+
         int count = 0;
         int longestCount = 0;
-        boolean match;
-        while(i <= sequence.length() - STR.length()) {
-            match = true;
-            for(int j = 0; j < STR.length(); j++) {
-                if(sequence.charAt(i + j) != STR.charAt(j)) {
-                    match = false;
-                    count = 0;
-                    break;
-                }
-            }
-            if(match) {
+
+        for (int i = m; i < n; i++) {
+            if (strHash == seqHash) {
+                i += (m - 1);
+                // begin checking for consecutive appearances
                 count++;
                 if(count > longestCount) {
                     longestCount = count;
                 }
-                i += STR.length();
             }
             else {
-                i++;
+                count = 0;
             }
+            seqHash = hash(sequence.substring(1 + (i - m), i));
+
         }
-        return longestCount;
+    return longestCount;
+    }
+
+    public static int hash(String s) {
+        if(s.isEmpty()) {
+            return 0;
+        }
+        return (int) (hash(s.substring(1)) + s.charAt(0) * Math.pow(EXTENDED_ASCII, s.length() - 1) % P);
     }
 }
